f3f5126271d792908b7e920cfc0967e9d15c6747
meichthys <10717998+meichthys@users.noreply.github.com>
2021-03-23 01:33:06 -0400

    Fixes Chapters over 20 are not handled correctly #2 and Fixes Handle books without chapters #6

--- src/mic.py
+++ src/mic.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import argparse
 import os
 import queue
 import sounddevice as sd
@@ -11,143 +10,192 @@ import requests
 from utils import str2int
 
 def start():
-    q = queue.Queue()
-
-    def int_or_str(text):
-        """Helper function for argument parsing."""
-        try:
-            return int(text)
-        except ValueError:
-            return text
+    stream = queue.Queue()
 
     def callback(indata, frames, time, status):
         """This is called (from a separate thread) for each audio block."""
         if status:
             print(status, file=sys.stderr)
-        q.put(bytes(indata))
-
-    parser = argparse.ArgumentParser(add_help=False)
-    parser.add_argument(
-        '-l', '--list-devices', action='store_true',
-        help='show list of audio devices and exit')
-    args, remaining = parser.parse_known_args()
-    if args.list_devices:
-        print(sd.query_devices())
-        parser.exit(0)
-    parser = argparse.ArgumentParser(
-        description=__doc__,
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        parents=[parser])
-    parser.add_argument(
-        '-f', '--filename', type=str, metavar='FILENAME',
-        help='audio file to store recording to')
-    parser.add_argument(
-        '-m', '--model', type=str, metavar='MODEL_PATH',
-        help='Path to the model')
-    parser.add_argument(
-        '-d', '--device', type=int_or_str,
-        help='input device (numeric ID or substring)')
-    parser.add_argument(
-        '-r', '--samplerate', type=int, help='sampling rate')
-    args = parser.parse_args(remaining)
+        stream.put(bytes(indata))
 
     try:
-        if args.model is None:
-            args.model = "model"
-        if not os.path.exists(args.model):
-            print ("Please download a model for your language from https://alphacephei.com/vosk/models")
-            print ("and unpack as 'model' in the current folder.")
-            parser.exit(0)
-        if args.samplerate is None:
-            device_info = sd.query_devices(args.device, 'input')
-            # soundfile expects an int, sounddevice provides a float:
-            args.samplerate = int(device_info['default_samplerate'])
-
-        model = vosk.Model(args.model)
-
-        if args.filename:
-            dump_fn = open(args.filename, "wb")
-        else:
-            dump_fn = None
-        with sd.RawInputStream(samplerate=args.samplerate, blocksize = 8000, device=args.device, dtype='int16',
+        model = load_model()
+        # Use first device by default: soundfile expects an int, sounddevice provides a float:
+        samplerate = int(sd.query_devices(0, 'input')['default_samplerate'])
+        # Start Input
+        with sd.RawInputStream(samplerate=samplerate, blocksize = 8000, device=0, dtype='int16',
                                 channels=1, callback=callback):
-                print('#' * 80)
-                print('Press Ctrl+C to stop the recording')
-                print('#' * 80)
+            print('#' * 80)
+            print('Press Ctrl+C to stop the recording')
+            print('#' * 80)
 
-                books = ["genesis","exodus","leviticus","numbers","deuteronomy","joshua","judges","ruth","1 samuel","2 samuel","1 kings","2 kings","1 chronicles","2 chronicles","ezra","nehemiah","esther","job","psalms","proverbs","ecclesiastes","song of solomon","isaiah","jeremiah","lamentations","ezekiel","daniel","hosea","joel","amos","obadiah","jonah","micah","nahum","habakkuk","zephaniah","haggai","zechariah","malachi ","matthew","mark","luke","john","the acts","romans","1 corinthians","2 corinthians","galatians","ephesians","philippians","colossians","1 thessalonians","2 thessalonians","1 timothy","2 timothy","titus","philemon","hebrews","james","1 peter","2 peter","1 john","2 john","3 john","jude","revelation"]
-                rec = vosk.KaldiRecognizer(model, args.samplerate)
-                while True:
-                    data = q.get()
-                    # Get recognized audio
-                    if rec.AcceptWaveform(data):
-                        latest = rec.PartialResult()
-                        rec.Result()
-                        # Parse partial text into a list of words and remove references to 'chapter' and 'verse'
-                        current = eval(latest)["partial"].replace("chapter ", "").replace("verse ", "").replace("number ", "").split()
-                        # Cycle through words looking for book titles
-                        word_count = 0
-                        for word in current:
-                            # Default chapter to be 1 word after the book title
-                            chapter_index = 1
-                            # Default verse to be 2 words after the book title
-                            verse_index = 2
-                            if ((word in books)
-                                or (word == "song")
-                                or (word == "first")
-                                or (word == "second")
-                                or (word == "third")
-                            ):
-                                # Consider the word to be a book title
-                                book = word
-                                # Handle Song of Solomon differently since it is three words
-                                if book == "song":
-                                    # Make sure entire book title was parsed
-                                    if not current[word_count+1:word_count+3] == ["of", "solomon"]:
-                                        continue
-                                    book = "song of solomon"
-                                    chapter_index = 3
-                                    verse_index = 4
-                                # Handle books with 'first'/'second'/'third' in title differently
-                                if book == "first" or book == "second" or book == "third":
-                                    # Adjust Chapter/Verse index
-                                    chapter_index = 2
-                                    verse_index = 3
-                                    # Adjust Book Title
-                                    if book == "first":
-                                        book = f"1 {current[word_count+1]}"
-                                    elif book == "second":
-                                        book = f"2 {current[word_count+1]}"
-                                    elif book == "third":
-                                        book = f"3 {current[word_count+1]}"
-                                    # Verify parsed book exists
-                                    if not book in books:
-                                        continue
-                                # Get chapter & verse numbers
-                                try:
-                                    chapter = str2int(current[word_count + chapter_index].replace("for", "four").replace("to", "two"))
-                                except:
-                                    chapter = ""
-                                try:
-                                    verse = str2int(current[word_count + verse_index].replace("for", "four").replace("to", "two"))
-                                except:
-                                    verse = ""
-                                if chapter == "":
-                                    continue
-                                # Show entire chapter if no verse is given
-                                if verse == "":
-                                    api_url = f"https://bible-api.com/{book}+{chapter}"
-                                else:
-                                    api_url = f"https://bible-api.com/{book}+{chapter}:{verse}"
-                                text = requests.get(api_url).json()["text"]
-                                print(f"{book.title()} {chapter}:{verse} \n {text}")
-                            word_count += 1
-
-                    if dump_fn is not None:
-                        dump_fn.write(data)
+            # Start speech recognition
+            start_recognition(model, samplerate, stream)
 
     except KeyboardInterrupt:
         print('\nDone')
-        parser.exit(0)
+
     except Exception as e:
-        parser.exit(type(e).__name__ + ': ' + str(e))
+        print(e)
+
+
+def load_model():
+    """Load required inputs for speech recognition"""
+    model_name = "model"
+    if not os.path.exists(model_name):
+        print ("Please download a model for your language from https://alphacephei.com/vosk/models")
+        print ("and unpack as 'model' in the current folder.")
+
+    return vosk.Model(model_name)
+
+def parse_stream(recognizer, stream):
+    """Parse recognized text for Bible references
+    ARGS:
+        recognizer: the vosk recognizer
+        stream: stream of recognized text
+    """
+    # Define book names (used as triggers to start looking for chapters/verses)
+    books = ["genesis","exodus","leviticus","numbers","deuteronomy","joshua","judges","ruth","1 samuel","2 samuel","1 kings","2 kings","1 chronicles","2 chronicles","ezra","nehemiah","esther","job","psalms","proverbs","ecclesiastes","song of solomon","isaiah","jeremiah","lamentations","ezekiel","daniel","hosea","joel","amos","obadiah","jonah","micah","nahum","habakkuk","zephaniah","haggai","zechariah","malachi ","matthew","mark","luke","john","the acts","romans","1 corinthians","2 corinthians","galatians","ephesians","philippians","colossians","1 thessalonians","2 thessalonians","1 timothy","2 timothy","titus","philemon","hebrews","james","1 peter","2 peter","1 john","2 john","3 john","jude","revelation"]
+
+    # If recording is still active, skip parsing
+    if not recognizer.AcceptWaveform(stream.get()):
+        return
+    # If recording has finished a chunk, then parse
+    raw = eval(recognizer.PartialResult())['partial'].split()
+    recognizer.Result()
+    # Filler words to be ignored durring parsing
+    chapter_filler_words = ["chapter"]
+    verse_filler_words = ["verse", "and"]
+    # Cycle through words looking for book titles
+    word_count = 0
+    for word in raw:
+        if ((word in books)
+            or (word == "song")
+            or (word == "first")
+            or (word == "second")
+            or (word == "third")
+        ):
+            # Consider the word to be a book title
+            book = word
+            # Default chapter to be 1 word after the book title
+            chapter_index = 1
+            # Handle Song of Solomon differently since it is three words
+            if book == "song":
+                # Make sure entire book title was parsed
+                if not raw[word_count+1:word_count+3] == ["of", "solomon"]:
+                    word_count += 1
+                    continue
+                book = "song of solomon"
+                chapter_index = 3
+            # Handle books with 'first'/'second'/'third' in title differently
+            if book == "first" or book == "second" or book == "third":
+                # Adjust Book Title
+                if book == "first":
+                    book = f"1 {raw[word_count+1]}"
+                    # remove the book from the text to prevent book of John from also showing
+                    raw.remove(raw[word_count+1])
+                elif book == "second":
+                    book = f"2 {raw[word_count+1]}"
+                    # remove the book from the text to prevent book of John from also showing
+                    raw.remove(raw[word_count+1])
+                elif book == "third":
+                    book = f"3 {raw[word_count+1]}"
+                    # remove the book from the text to prevent book of John from also showing
+                    raw.remove(raw[word_count+1])
+                # Verify parsed book exists
+                if not book in books:
+                    word_count += 1
+                    continue
+
+            # Get amount of numbers after Book is mentioned
+            chapter_filler_word_count = 0
+            verse_filler_word_count = 0
+            reference_items = 0
+            number = 0
+            while number < 4 + chapter_filler_word_count + verse_filler_word_count:
+                try:
+                    if raw[word_count+chapter_index+number] in chapter_filler_words:
+                        chapter_filler_word_count += 1
+                        number += 1
+                        continue
+                    elif raw[word_count+chapter_index+number] in verse_filler_words:
+                        verse_filler_word_count += 1
+                        number += 1
+                        continue
+                    else:
+                        if str2int(raw[word_count+chapter_index+number].replace("for", "four").replace("to", "two").replace("fourty", "forty").replace("fourty", "forty")) != "":
+                            reference_items += 1
+                        number += 1
+                except:
+                    break
+            # If only one number is referenced continue unless book has one chapter
+            if reference_items == 1:
+                # If book has only one chapter, then assume the number is a verse
+                try:
+                    if book in ["obadiah", "philemon", "jude", "2 john", "3 john"]:
+                        chapter = 1
+                        verse = str2int(raw[word_count+chapter_index].replace("for", "four").replace("to", "two").replace("fourty", "forty"))
+                    else:
+                        word_count += 1
+                        continue
+                except:
+                    word_count += 1
+                    continue
+            # If two numbers are referenced, assume first is chapter and
+            #   second is verse unless first is a multiple of 10, then continue
+            elif reference_items == 2:
+                try:
+                    if raw[word_count+chapter_index+chapter_filler_word_count].endswith("ty") and verse_filler_word_count > 0:
+                        chapter = str2int(raw[word_count+chapter_index+chapter_filler_word_count].replace("for", "four").replace("to", "two").replace("fourty", "forty"))
+                        verse = str2int(raw[word_count+chapter_index+chapter_filler_word_count+1+verse_filler_word_count].replace("for", "four").replace("to", "two").replace("fourty", "forty"))
+                    else:
+                        chapter = str2int(raw[word_count+chapter_index+chapter_filler_word_count].replace("for", "four").replace("to", "two").replace("fourty", "forty"))
+                        verse = str2int(raw[word_count+chapter_index+chapter_filler_word_count+1+verse_filler_word_count].replace("for", "four").replace("to", "two").replace("fourty", "forty"))
+                except:
+                    word_count += 1
+                    continue
+            elif reference_items == 3:
+                try:
+                    if raw[word_count+chapter_index+chapter_filler_word_count].endswith("ty") and chapter_filler_word_count > 0:
+                        chapter = str2int(raw[word_count+chapter_index+chapter_filler_word_count].replace("for", "four").replace("to", "two").replace("fourty", "forty"))
+                        verse = str2int(raw[word_count+chapter_index+chapter_filler_word_count+1+verse_filler_word_count].replace("for", "four").replace("to", "two").replace("fourty", "forty"))+str2int(raw[word_count+chapter_index+chapter_filler_word_count+1+verse_filler_word_count+1].replace("for", "four").replace("to", "two").replace("fourty", "forty"))
+                    elif raw[word_count+chapter_index+chapter_filler_word_count].endswith("ty") and verse_filler_word_count == 0:
+                        chapter = str2int(raw[word_count+chapter_index+chapter_filler_word_count].replace("for", "four").replace("to", "two").replace("fourty", "forty"))+str2int(raw[word_count+chapter_index+chapter_filler_word_count+1].replace("for", "four").replace("to", "two").replace("fourty", "forty"))
+                        verse = str2int(raw[word_count+chapter_index+chapter_filler_word_count+1+verse_filler_word_count+1].replace("for", "four").replace("to", "two").replace("fourty", "forty"))
+                    # elif verse_filler_word_count >= 0:
+                    #     chapter = str2int(raw[word_count+chapter_index+chapter_filler_word_count].replace("for", "four").replace("to", "two").replace("fourty", "forty"))
+                    #     verse = str2int(raw[word_count+chapter_index+chapter_filler_word_count+1+verse_filler_word_count].replace("for", "four").replace("to", "two").replace("fourty", "forty"))+str2int(raw[word_count+chapter_index+chapter_filler_word_count+1+verse_filler_word_count+1].replace("for", "four").replace("to", "two").replace("fourty", "forty"))
+                    else:
+                        chapter = str2int(raw[word_count+chapter_index+chapter_filler_word_count].replace("for", "four").replace("to", "two").replace("fourty", "forty"))+str2int(raw[word_count+chapter_index+chapter_filler_word_count+1].replace("for", "four").replace("to", "two").replace("fourty", "forty"))
+                        verse = str2int(raw[word_count+chapter_index+chapter_filler_word_count+1+verse_filler_word_count+1].replace("for", "four").replace("to", "two").replace("fourty", "forty"))
+                except:
+                    word_count += 1
+                    continue
+            elif reference_items == 4:
+                try:
+                    chapter = str2int(raw[word_count+chapter_index+chapter_filler_word_count].replace("for", "four").replace("to", "two").replace("fourty", "forty"))+str2int(raw[word_count+chapter_index+chapter_filler_word_count+1].replace("for", "four").replace("to", "two").replace("fourty", "forty"))
+                    verse = str2int(raw[word_count+chapter_index+chapter_filler_word_count+1+verse_filler_word_count].replace("for", "four").replace("to", "two").replace("fourty", "forty"))+str2int(raw[word_count+chapter_index+chapter_filler_word_count+1+verse_filler_word_count+1].replace("for", "four").replace("to", "two").replace("fourty", "forty"))
+                except:
+                    word_count += 1
+                    continue
+            else:
+                word_count += 1
+                continue
+            if verse == "":
+                api_url = f"https://bible-api.com/{book}+{chapter}"
+            else:
+                api_url = f"https://bible-api.com/{book}+{chapter}:{verse}"
+            text = requests.get(api_url).json()["text"]
+            print(f"{book.title()} {chapter}:{verse} \n {text}")
+        word_count += 1
+
+def start_recognition(model, samplerate, stream):
+    """Start speech recognition
+    ARGS:
+        model: vosk speech recognition model
+        samplerate: integer containing the sample rate
+        stream: the audio stream
+    """
+    recognizer = vosk.KaldiRecognizer(model, samplerate)
+    while True:
+        parse_stream(recognizer, stream)
